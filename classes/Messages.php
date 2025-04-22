b6b896bafe236d5e66102af427f5f3a2c48a4640
Sarah Pindstoft Steenbjerg <ss24001@tietgen.dk>
2025-04-22 14:08:34 +0200

    opgave 1 løst

--- classes/Messages.php
+++ classes/Messages.php
@@ -1,1 +1,26 @@
 <?php
+
+/**
+ * Class Messages
+ * 
+ * Messages object for printing messages
+ * 
+ * @method string print_tintytexy
+ */
+class Messages {
+
+    /**
+     * Returns message if it has a max of 255 otherwise throws an exception
+     *
+     * @param string $message
+     * @return string
+     */
+    public static function print_tinytext($message) {
+        $len = strlen($message);
+        if ($len > 255) {
+            throw new Exception("der må maks bruges 255 tegn");
+        } else {
+            return $message;
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+
+require_once 'classes/Messages.php';
+
+//Prints text if max amount of letters is 255 or cataches the exception
+try {
+    echo Messages::print_tinytext("Lorem ipsum dolor sit amet");
+} catch(Exception $e) {
+    echo "Fejl: " .$e->getMessage();
+}
