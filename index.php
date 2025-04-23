f4f9b5292002855732db182a3a6c1c8cc36f88e2
Sarah Pindstoft Steenbjerg <ss24001@tietgen.dk>
2025-04-23 09:44:09 +0200

    opgave 1 løst

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 require_once 'classes/Messages.php';
 
-//Prints text if max amount of letters is 255 or cataches the exception
+//Udskriver teksten hvis der er maks 255 tegn ellers fanger den exception
 try {
-    echo Messages::print_tinytext("Lorem ipsum dolor sit amet");
+    echo Messages::print_tinytext("Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, voluptate cum! Sapiente est sit aliquam maxime, ut explicabo temporibus quo maiores incidunt rerum repellat assumenda non quasi pariatur accusantium nihil Lorem, ips");
 } catch(Exception $e) {
-    echo "Fejl: " .$e->getMessage();
+    echo $e->getMessage();
 }
